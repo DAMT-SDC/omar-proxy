21125a39ee4f19a305c10eaf1a87f90be72143ea
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-11-06 19:40:57 -0800

    got the proxy server rendering my module withouth right font

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+const cors = require('cors')
 const port = 4000;
 
 app.use(morgan('dev'));
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+ 
